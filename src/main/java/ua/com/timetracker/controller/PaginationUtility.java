92cd0f3cf3a35bdb3b91120c90aeab70356248fc
Игорь Волчков <ihor.volchkov@gmail.com>
2019-12-04 23:58:08 +0200

    Add javadoc for pagination interface

--- src/main/java/ua/com/timetracker/controller/PaginationUtility.java
+++ src/main/java/ua/com/timetracker/controller/PaginationUtility.java
@@ -6,7 +6,21 @@ import ua.com.timetracker.exception.InvalidPaginatingException;
 
 import java.util.Objects;
 
+/**
+ * Interface which is needed for pagination.
+ * Provides two default methods for pagination and validation of parameters
+ *
+ * @author Ihor Volchkov
+ **/
 public interface PaginationUtility {
+    /**
+     * Returns String which represents what url will be shown.
+     *
+     * @param currentPage    element which set current page for pagination.
+     * @param recordsPerPage element which set records per page for pagination.
+     * @param modelAndView   element who is needed to set appropriate attributes.
+     * @param rows           element which set number of rows for pagination.
+     */
     default void paginating(Integer currentPage, Integer recordsPerPage, ModelAndView modelAndView,
                             String page, long rows) {
         long numberOfPages = rows / recordsPerPage;
@@ -22,6 +36,13 @@ public interface PaginationUtility {
         modelAndView.setViewName(page);
     }
 
+    /**
+     * Validate parameters for pagination. They shouldn`t have null value.
+     *
+     * @param currentPage    element which is validating.
+     * @param recordsPerPage element which is validating.
+     * @throws InvalidPaginatingException if the current page or records per page is null.
+     */
     default void paginatingValidation(Integer currentPage, Integer recordsPerPage) {
         if (Objects.isNull(currentPage) || Objects.isNull(recordsPerPage)) {
             throw new InvalidPaginatingException("Current page or records per page are invalid!");
